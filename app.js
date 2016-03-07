56dc422133f741ee1fb754b00b076047634b9a66
angamanga <anna.iosif@me.com>
2016-03-07 22:08:50 +0000

    sending data on ws-connect

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+"use strict";
 const WebSocketServer = require("ws").Server;
 const httpServer = require("./src/lib/http-server");
 const actions = require("./src/store/actions");
@@ -24,7 +25,8 @@ store.subscribe(
 wss.broadcast = data => wss.clients.forEach(client => client.send(data));
 
 wss.on("connection", ws => {
-  const action = JSON.stringify({type: "WS_CONNECTED"});
+  const data = store.getState();
+  const action = JSON.stringify({type: "BACKEND_DATA", data});
   ws.send(action);
 
   ws.on("message", message => {
@@ -32,6 +34,7 @@ wss.on("connection", ws => {
       const action = JSON.parse(message);
       console.log("Received action from client:");
       console.log(action);
+      
       store.dispatch(action);
     } catch (e) {
       console.error(e.message);
